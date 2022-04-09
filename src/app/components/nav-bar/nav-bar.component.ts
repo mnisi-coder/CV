0026ddfad44dc2aa975d83c23f92c61a6fc46457
Jali Mnisi <jalimnisi@Jalis-MacBook-Air.local>
2022-04-09 12:44:40 +0200

    nav

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
+
+
 export class NavBarComponent implements OnInit {
 
+  @HostListener('window:scroll', ['$event'])
+  
+  isScrolled = false;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onWindowScroll() {
+    let element = document.querySelector('.nav') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbarShow');
+    } else {
+      element.classList.remove('navbarShow');
+    }
+  }
+ 
 }
